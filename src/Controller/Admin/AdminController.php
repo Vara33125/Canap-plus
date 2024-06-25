5575dd33944d498e0bc04fe389653a02ec20d7a7
Vara33125 <vincent.kherfallah@gmail.com>
2024-06-25 09:46:32 +0200

    adminboard_view_created

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -3,8 +3,13 @@
 
 namespace App\Controller\Admin;
 
-
-
+use App\Repository\CategoryRepository;
+use App\Repository\ContactRepository;
+use App\Repository\ProductRepository;
+use App\Repository\StoreRepository;
+use App\Repository\TagRepository;
+use App\Repository\UserRepository;
+use Gedmo\Tree\RepositoryInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,9 +18,41 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/admin')]
 class AdminController extends AbstractController
 {
+    public function __construct(private ProductRepository $productRepository,private CategoryRepository $categoryRepository ,private TagRepository $tagRepository ,private UserRepository $userRepository ,private StoreRepository $storeRepository,private ContactRepository $contactRepository)
+    {
+
+    }
     #[Route('/', name: 'app_admin_home', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('pages/admin/home/index.html.twig');
+        $categories = $this->categoryRepository->findAll();
+        $nbrcat = count($categories);
+       
+        $products = $this->productRepository->findAll();
+        $nbrProducts = count($products);
+
+        $tags = $this->tagRepository->findAll();
+        $nbrTag = count($tags);
+
+        $users = $this->userRepository->findAll();
+        $nbrUser = count($users);
+
+        $stores = $this->storeRepository->findAll();
+        $nbrStores = count($stores);
+
+        $contacts = $this->contactRepository->findAll();
+        $nbrContacts = count($contacts);
+
+        return $this->render('pages/admin/home/index.html.twig', [
+            'categorie' => $nbrcat,
+            'product' => $nbrProducts,
+            'tag' => $nbrTag,
+            'user' => $nbrUser,
+            'store' => $nbrStores,
+            'contact' => $nbrContacts
+
+        ]);
     }
+
+    
 }
